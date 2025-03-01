cc7e59f201b7fbc13688bf0dace29631a657e0a2
Crawlerbeast-m <manishr841@gmail.com>
2025-03-01 23:28:20 +0530

    First commit

---
+++ JavaScript/script.js
@@ -0,0 +1,10 @@
+function downloadResume() {
+    // Specify the path to your resume file
+    const resumeUrl = '../Devops_Engineer_Manish.pdf';  // Replace with your actual file path
+    const a = document.createElement('a');
+    a.href = resumeUrl;
+    a.download = 'Manish_Rawat.pdf';  // The filename to save as
+    document.body.appendChild(a); // Append the link to the body
+    a.click();  // Trigger the click event to start the download
+    document.body.removeChild(a);
+}
