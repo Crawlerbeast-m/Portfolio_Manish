a7aebe9ae4d0a3b0096b56f476099162d9925542
Crawlerbeast-m <manishr841@gmail.com>
2025-03-20 20:30:53 +0530

    changes last updates

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function downloadResume() {
     // Specify the path to your resume file
-    const resumeUrl = '../Devops_Engineer_Manish.pdf';  // Replace with your actual file path
+    const resumeUrl = 'Devops_Engineer_Manish.pdf';  // Replace with your actual file path
     const a = document.createElement('a');
     a.href = resumeUrl;
     a.download = 'Manish_Rawat.pdf';  // The filename to save as
